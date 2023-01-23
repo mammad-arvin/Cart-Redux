654a7eb7c59f6d0468d06b47185c64421fcdfe3f
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-01-23 22:23:33 +0330

    disable checkout form when user cart is empty

--- src/components/products/Cart.js
+++ src/components/products/Cart.js
@@ -54,77 +54,80 @@ const Cart = () => {
                             </>
                     }
                 </div>
-    
-                <div className={style.rightSile}>
-                    {
-                        !cart_catfilter.cart_catfilter.checkout?
-                            <>
-                                    <div className={style.getAddress}>
-                                    <h3>CHECKOUT</h3>
-                                    <div className={style.delicConst}><p>Delivery Cost:</p>  <span>$8.6</span></div>
-                                    <label htmlFor="Province">Province:</label>
-                                    <select name='Province' onChange={(event)=> dispach({type:event.target.value})}>
-                                        <option value="Select Province">select Province</option>
-                                        <option value="Tehran" >Tehran</option>
-                                        <option value="Zagros" >Zagros</option>
-                                        <option value="Fars" >Fars</option>
-                                    </select>
-                                    <label htmlFor="City">City:</label>
-                                    <select name='City'>
-                                        <option value="Select City">select...</option>
-                                        {
-                                            cart_catfilter.cart_catfilter.Province ==="Tehran" ?
-                                                <>
-                                                    <option value="Tehran">Tehran</option>
-                                                    <option value="karaj">karaj</option>
-                                                    <option value="shahr ghods">shahr ghods</option>
-                                                </>:
-                                            cart_catfilter.cart_catfilter.Province ==="Zagros" ?
-                                                <>
-                                                    <option value="Yasuj">Yasuj</option>
-                                                    <option value="sisakht">sisakht</option>
-                                                    <option value="dehdasht">dehdasht</option>
-                                                </>:
-                                            cart_catfilter.cart_catfilter.Province ==="Fars" ?
-                                                <>
-                                                    <option value="shiraz">shiraz</option>
-                                                    <option value="sadra">sadra</option>
-                                                </>:
-                                            null
-                
-                                        }
-                                    </select>
-                                    <label htmlFor="Zip">Zip/Postcode:</label>
-                                    <input type="text" name="Zip" placeholder="zip/Postcode" maxLength={10} />
-                                    
-                                </div>
-                                <div className={style.checkuot}>
-                                    <div className={style.totalItems}>
-                                        <h3>Total Items:</h3>
-                                        <h3>{TotalItems.split(" ")[1]}</h3>
+                    
+                {
+                    cart_catfilter.cart_catfilter.itemCounter ?
+                    <div className={style.rightSile}>
+                        {
+                            !cart_catfilter.cart_catfilter.checkout?
+                                <>
+                                        <div className={style.getAddress}>
+                                        <h3>CHECKOUT</h3>
+                                        <div className={style.delicConst}><p>Delivery Cost:</p>  <span>$8.6</span></div>
+                                        <label htmlFor="Province">Province:</label>
+                                        <select name='Province' onChange={(event)=> dispach({type:event.target.value})}>
+                                            <option value="Select Province">select Province</option>
+                                            <option value="Tehran" >Tehran</option>
+                                            <option value="Zagros" >Zagros</option>
+                                            <option value="Fars" >Fars</option>
+                                        </select>
+                                        <label htmlFor="City">City:</label>
+                                        <select name='City'>
+                                            <option value="Select City">select...</option>
+                                            {
+                                                cart_catfilter.cart_catfilter.Province ==="Tehran" ?
+                                                    <>
+                                                        <option value="Tehran">Tehran</option>
+                                                        <option value="karaj">karaj</option>
+                                                        <option value="shahr ghods">shahr ghods</option>
+                                                    </>:
+                                                cart_catfilter.cart_catfilter.Province ==="Zagros" ?
+                                                    <>
+                                                        <option value="Yasuj">Yasuj</option>
+                                                        <option value="sisakht">sisakht</option>
+                                                        <option value="dehdasht">dehdasht</option>
+                                                    </>:
+                                                cart_catfilter.cart_catfilter.Province ==="Fars" ?
+                                                    <>
+                                                        <option value="shiraz">shiraz</option>
+                                                        <option value="sadra">sadra</option>
+                                                    </>:
+                                                null
+                    
+                                            }
+                                        </select>
+                                        <label htmlFor="Zip">Zip/Postcode:</label>
+                                        <input type="text" name="Zip" placeholder="zip/Postcode" maxLength={10} />
+                                        
                                     </div>
-                                    <div className={style.balance}>
-                                        <h3>Balance<span> (USD)</span>:</h3>
-                                        <h3>${balance.toFixed(2)}</h3>
-                                    </div>
-                                    <button 
-                                        onClick={()=> dispach({type:"checkout"})}
-                                        className={style.checkuotBtn}>CHECKOUT</button>
-                                    <hr />
-                                    <button
-                                        onClick={()=> dispach({type:"clearCart"})}
-                                        className={style.clearCartBtn}>CLEAR CART</button>
-                                </div>
-                            </>:
-                                    <>
-                                        <div className={style.checkSuccess}>
-                                            <h3>Check Out Successfully</h3>
-                                            <Link to="/"  onClick={()=> dispach({type:"RefalseCheckout"})}>Back to home page</Link>
+                                    <div className={style.checkuot}>
+                                        <div className={style.totalItems}>
+                                            <h3>Total Items:</h3>
+                                            <h3>{TotalItems.split(" ")[1]}</h3>
                                         </div>
-                                    </>
-                               
-                    }
-                </div>
+                                        <div className={style.balance}>
+                                            <h3>Balance<span> (USD)</span>:</h3>
+                                            <h3>${balance.toFixed(2)}</h3>
+                                        </div>
+                                        <button 
+                                            onClick={()=> dispach({type:"checkout"})}
+                                            className={style.checkuotBtn}>CHECKOUT</button>
+                                        <hr />
+                                        <button
+                                            onClick={()=> dispach({type:"clearCart"})}
+                                            className={style.clearCartBtn}>CLEAR CART</button>
+                                    </div>
+                                </>:
+                                        <>
+                                            <div className={style.checkSuccess}>
+                                                <h3>Check Out Successfully</h3>
+                                                <Link to="/"  onClick={()=> dispach({type:"RefalseCheckout"})}>Back to home page</Link>
+                                            </div>
+                                        </>
+                                
+                        }
+                    </div> : null
+                } 
             </div>
         );
     
